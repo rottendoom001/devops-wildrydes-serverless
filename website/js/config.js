c1be3ee516dc67a03db4f0e12facc06b96032e7a
Ubuntu <ubuntu@ip-172-31-61-1.ec2.internal>
2020-04-16 01:05:25 +0000

    Ejercicio completo

---
+++ website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-1_hjci6HPMM",
+    userPoolClientId: "7jisa59fgp7592jcqtbc2o3pcu", 
+    region: "us-east-1" // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "https://xj2fxan7f0.execute-api.us-east-1.amazonaws.com/dev" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+  }
+};
